d47b56db0a4562a9f43e03c6d3f16cd514c8c1bf
Steven Kay <stevendkay@gmail.com>
2019-01-16 14:44:47 +0000

    initial commit, added Till

---
+++ src/main/java/Till.java
@@ -0,0 +1,15 @@
+public class Till {
+	double Amount;
+
+	public Till(double amount) {
+		Amount = amount;
+	}
+
+	public double getAmount() {
+		return Amount;
+	}
+
+	public void setAmount(double amount) {
+		Amount = amount;
+	}
+}
---
+++ src/test/java/TillTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TillTest {
+
+	private Till till;
+
+	@Before
+	public void setUp() throws Exception {
+		till = new Till(100.0);
+	}
+
+	@Test
+	public void getAmount() {
+		assertEquals(100.0,till.getAmount(),.001);
+	}
+
+	@Test
+	public void setAmount() {
+		till.setAmount(200.0);
+		assertEquals(200.0, till.getAmount(), 0.001);
+	}
+}
