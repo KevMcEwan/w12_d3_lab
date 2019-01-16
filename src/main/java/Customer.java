8b6f37edf7ed51ad44ab3726c2104cacdb4e8fd2
Steven Kay <stevendkay@gmail.com>
2019-01-16 14:55:12 +0000

    customer and tests

---
+++ src/main/java/Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+	private String name;
+	private double budget;
+
+	public Customer(String name, double budget) {
+		this.name = name;
+		this.budget = budget;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBudget() {
+		return budget;
+	}
+
+	public void setBudget(double budget) {
+		this.budget = budget;
+	}
+}
---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+	private Customer customer;
+
+	@Before
+	public void setUp() throws Exception {
+		customer = new Customer("Buck Rodgers", 10000.0);
+	}
+
+	@Test
+	public void getName() {
+		assertEquals("Buck Rodgers", customer.getName());
+	}
+
+	@Test
+	public void setName() {
+		customer.setName("Ducky Dodgers");
+		assertEquals("Ducky Dodgers", customer.getName());
+	}
+
+	@Test
+	public void getBudget() {
+		assertEquals(10000.0, customer.getBudget(), .0001);
+	}
+
+	@Test
+	public void setBudget() {
+		customer.setBudget(20000.0);
+		assertEquals(20000.0, customer.getBudget(), .001);
+	}
+}
